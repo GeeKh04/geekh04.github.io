a4e1adedb9a0bb6899e32fade0b9709b331581f5
geekh04 <rida95kh@gmail.com>
2020-07-21 23:03:38 +0100

    lang

--- lang/translate.js
+++ lang/translate.js
@@ -9,7 +9,7 @@ function Translate() {
                 _self = this;
                 var xrhFile = new XMLHttpRequest();
                 //load content data 
-                xrhFile.open("GET", "./lang/"+this.lng+".json", false);
+                xrhFile.open("GET", "./lang/"+this.lng+".json", true);
                 xrhFile.onreadystatechange = function ()
                 {
                     if(xrhFile.readyState === 4)
@@ -17,14 +17,12 @@ function Translate() {
                         if(xrhFile.status === 200 || xrhFile.status == 0)
                         {
                             var LngObject = JSON.parse(xrhFile.responseText);
-                            console.log(LngObject["name1"]);
                             var allDom = document.getElementsByTagName("*");
                             for(var i =0; i < allDom.length; i++){
                                 var elem = allDom[i];
                                 var key = elem.getAttribute(_self.attribute);
                                  
                                 if(key != null) {
-                                     console.log(key);
                                      elem.innerHTML = LngObject[key]  ;
                                 }
                             }
